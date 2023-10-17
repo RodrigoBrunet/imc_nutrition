c3b0be7a8ff2bb8a8fc6d5b409b7e1fddef430e8
Rodrigo Brunet <rodrigosistemas2016@gmail.com>
2023-10-17 18:07:46 -0300

    creating height input component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc_nutrition/widgets/input_height.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,11 +7,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text('Calculadora de IMC'),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 52,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Calculadora de IMC',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              children: [
+                InputHeight(
+                  label: 'Height',
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                InputHeight(
+                  label: 'Weight',
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
